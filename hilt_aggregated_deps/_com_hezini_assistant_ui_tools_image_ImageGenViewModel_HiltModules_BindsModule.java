a9c75596a11c0743048be630bb7271f2d2480fbf
Lakki Reddy Bharath Simha REDDY <112234080+lakki8790@users.noreply.github.com>
2026-06-26 18:46:52 +0530

    Add files via upload

---
+++ hilt_aggregated_deps/_com_hezini_assistant_ui_tools_image_ImageGenViewModel_HiltModules_BindsModule.java
@@ -0,0 +1,15 @@
+package hilt_aggregated_deps;
+
+import dagger.hilt.processor.internal.aggregateddeps.AggregatedDeps;
+import javax.annotation.processing.Generated;
+
+/**
+ * This class should only be referenced by generated code! This class aggregates information across multiple compilations.
+ */
+@AggregatedDeps(
+    components = "dagger.hilt.android.components.ViewModelComponent",
+    modules = "com.hezini.assistant.ui.tools.image.ImageGenViewModel_HiltModules.BindsModule"
+)
+@Generated("dagger.hilt.processor.internal.aggregateddeps.AggregatedDepsGenerator")
+public class _com_hezini_assistant_ui_tools_image_ImageGenViewModel_HiltModules_BindsModule {
+}
---
+++ hilt_aggregated_deps/_com_hezini_assistant_ui_tools_writing_WritingFragment_GeneratedInjector.java
@@ -0,0 +1,15 @@
+package hilt_aggregated_deps;
+
+import dagger.hilt.processor.internal.aggregateddeps.AggregatedDeps;
+import javax.annotation.processing.Generated;
+
+/**
+ * This class should only be referenced by generated code! This class aggregates information across multiple compilations.
+ */
+@AggregatedDeps(
+    components = "dagger.hilt.android.components.FragmentComponent",
+    entryPoints = "com.hezini.assistant.ui.tools.writing.WritingFragment_GeneratedInjector"
+)
+@Generated("dagger.hilt.processor.internal.aggregateddeps.AggregatedDepsGenerator")
+public class _com_hezini_assistant_ui_tools_writing_WritingFragment_GeneratedInjector {
+}
