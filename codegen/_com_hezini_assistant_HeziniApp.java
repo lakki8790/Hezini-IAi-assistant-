de527c1ef444fb62940786b50385ea1d4a76cac5
Lakki Reddy Bharath Simha REDDY <112234080+lakki8790@users.noreply.github.com>
2026-06-26 18:44:54 +0530

    Add files via upload

---
+++ codegen/_com_hezini_assistant_HeziniApp.java
@@ -0,0 +1,21 @@
+package dagger.hilt.internal.aggregatedroot.codegen;
+
+import dagger.hilt.android.HiltAndroidApp;
+import dagger.hilt.internal.aggregatedroot.AggregatedRoot;
+import javax.annotation.processing.Generated;
+
+/**
+ * This class should only be referenced by generated code! This class aggregates information across multiple compilations.
+ */
+@AggregatedRoot(
+    root = "com.hezini.assistant.HeziniApp",
+    rootPackage = "com.hezini.assistant",
+    originatingRoot = "com.hezini.assistant.HeziniApp",
+    originatingRootPackage = "com.hezini.assistant",
+    rootAnnotation = HiltAndroidApp.class,
+    rootSimpleNames = "HeziniApp",
+    originatingRootSimpleNames = "HeziniApp"
+)
+@Generated("dagger.hilt.processor.internal.root.AggregatedRootGenerator")
+public class _com_hezini_assistant_HeziniApp {
+}
